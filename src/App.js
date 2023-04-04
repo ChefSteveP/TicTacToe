9d8444c6a2876dcfb228cec2bb8e10dfa785b21e
Stephen Pasch <sspasch2002@gmail.com>
2023-04-04 17:18:19 -0400

    finished the react tutorial

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function Square({value, onSquareClick}){
   );
 }
 
-
 function Board({XIsNext,squares, onPlay}) {
   
   function handleClick (i){
@@ -81,19 +80,36 @@ function calculateWinner(squares){
 } 
 
 export default function Game() {
-  const [XIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const currentSquares = history[history.length - 1];
+  const [currentMove, setCurrentMove] = useState(0);
+  const XIsNext = currentMove % 2 ===0;
+  const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares){
-      setHistory([...history, nextSquares]);
-      setXIsNext(!XIsNext);
+    const nextHistory = [...history.slice(0,currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
   }
-
+  
   function jumpTo(nextMove){
-    //
+    setCurrentMove(nextMove);
   }
 
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0){
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+
+    return (
+      <li key = {move}>
+        <button onClick = { () => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
 
   return(
     <div className='game'>
@@ -101,7 +117,7 @@ export default function Game() {
         <Board XIsNext = {XIsNext} squares = {currentSquares} onPlay = {handlePlay}/>
       </div>
       <div className='game-info'>
-        <ol>{}</ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
